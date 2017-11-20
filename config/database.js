2b04f0d2af393520efc6254a7941d01841c698f5
Donald Moore <donmoore26762671@gmail.com>
2017-11-20 09:34:30 -0500

    Got Angular running for a new client project. Learning the basics

---
+++ config/database.js
@@ -0,0 +1,12 @@
+// Provides cryptographic functionality (OpenSSL's hash, HMAC, cipher, decipher, sign and verify functions)
+var crypto = require('crypto').randomBytes(256).toString('hex'); 
+
+
+module.exports = {
+
+  uri: 'mongodb://localhost:27017/' + this.db, 
+
+  secret: crypto, 
+
+  db: 'mean-angular-2'
+}
--- index.js
+++ index.js
@@ -1,10 +1,34 @@
 var express = require('express');
 var app = express();
+var mongoose = require('mongoose');
+var config = require('./config/database');
+
+//mongoose.Promise = global.Promise;
+
+mongoose.connect(config.uri, 
+
+	{ useMongoClient: true, 
+
+	}, function(err){
+
+	if (err) {
+
+ 		console.log('Could not connect to database: ', err);
+	}else {
+
+		console.log('Connected to database: ' + config.db);
+
+	}
+});
 
 app.get('*', function (req, res) {
+
   res.send('<h1>Hello Daniel!</h1>');
+
 });
 
 app.listen(8080, function() {
+
 	console.log('Listening on port 8080');
+
 });

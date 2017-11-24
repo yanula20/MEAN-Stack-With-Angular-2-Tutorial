7b30917979fff69cc3ae843aca6bcb19a176a0ee
Donald Moore <donmoore26762671@gmail.com>
2017-11-24 15:56:31 -0500

    Completed e-mail and password validators for userSchema. Added custom err messages to authentication/register component.

--- models/user.js
+++ models/user.js
@@ -4,27 +4,119 @@ var Schema = mongoose.Schema;
 var bcrypt = require('bcrypt-nodejs');
 SALT_WORK_FACTOR = 10;
 
-var emailLengthChecker = function (email){
-    if (!email){
-        return false;
+
+var emailLengthChecker = function (email) {
+  // Check if e-mail exists
+  if (!email) {
+    return false; // Return error
+  } else {
+    // Check the length of e-mail string
+    if (email.length < 5 || email.length > 30) {
+      return false; // Return error if not within proper length
     } else {
-        if (email.length < 5 || email.length > 30){
-        return false;
-    } else {
-      return true;
+      return true; // Return as valid e-mail
     }
   }
 };
 
+// Validate Function to check if valid e-mail format
+var validEmailChecker = function (email) {
+  // Check if e-mail exists
+  if (!email) {
+    return false; // Return error
+  } else {
+    // Regular expression to test for a valid e-mail
+    var regExp = new RegExp(/^(([^<>()\[\]\\.,;:\s@"]+(\.[^<>()\[\]\\.,;:\s@"]+)*)|(".+"))@((\[[0-9]{1,3}\.[0-9]{1,3}\.[0-9]{1,3}\.[0-9]{1,3}])|(([a-zA-Z\-0-9]+\.)+[a-zA-Z]{2,}))$/);
+    return regExp.test(email); // Return regular expression test results (true or false)
+  }
+};
 
-var emailValidators = [{
-  validator: emailLengthChecker, message: 'Email must be at least 5 characters, but no more than 30.'
-}];
+// Array of Email Validators
+var emailValidators = [
+  // First Email Validator
+  {
+    validator: emailLengthChecker,
+    message: 'E-mail must be at least 5 characters but no more than 30'
+  },
+  // Second Email Validator
+  {
+    validator: validEmailChecker,
+    message: 'Must be a valid e-mail'
+  }
+];
+
+var usernameLengthChecker = function(username){
+    if(!username){
+        return false;
+    } else {
+        if(username.length < 5 || username.length > 15){
+            return false
+        } else{
+            return true;
+        }
+    }
+};
+
+var validUsername = function(username){
+    if(!username){
+        return false;
+    } else{
+        regExp = new RegExp(/^[a-zA-Z0-9]+$/);
+        return regExp.test(username);
+    }
+};
+
+usernameValidators = [
+   {
+    validator: usernameLengthChecker,
+    message: "Username must be between 5 and 15 characters in length."
+   },
+   {
+    validator: validUsername,
+    message: "Username must not have any special characters."
+   }
+];
+
+
+var passwordLengthChecker = function(password){
+    if(!password){
+        return false;
+    } else {
+        if (password.length < 8 || password.length > 35){
+            return false;
+        } else {
+            return true;
+        }
+    }
+
+};
+
+var validPaassword = function(password){
+    if(!password){
+        return false;
+    } else {
+        var regExp = new RegExp(/^(?=.*?[a-z])(?=.*?[A-Z])(?=.*?[\d])(?=.*?[\W]).{8,35}$/);
+        return regExp.test(password);
+    } 
+}
+
+var passwordValidators = [
+
+ {
+    validator: passwordLengthChecker,
+    message: 'Your password must be between 8 and 35 characters in length.'
+ },
+ {
+    validator: validPaassword,
+    message: 'Your password must have at least one uppercase, lowercase, special character, and number.'
+ }
+
+];
 
 var userSchema = new Schema({
   email: { type: String, required: true, unique: true, lowercase: true, validate: emailValidators },
-  username: { type: String, required: true, unique: true, lowercase: true },
-  password: { type: String, required: true }
+  username: { type: String, required: true, unique: true, lowercase: true, validate: usernameValidators },
+  password: { type: String, required: true, validate: passwordValidators}
 });
 
 
--- routes/authentication.js
+++ routes/authentication.js
@@ -31,32 +31,43 @@ router.post('/register', function (req, res){
 		  	password: req.body.password
 
 		  });
+		   user.save( function (err){
+            // Check if error occured
+            if (err) {
+              // Check if error is an error indicating duplicate account
+              if (err.code === 11000) {
+                res.json({ success: false, message: 'Username or e-mail already exists' }); // Return error
+              } else {
+                // Check if error is a validation rror
+                if (err.errors) {
+                  // Check if validation error is in the email field
+                  if (err.errors.email) {
+                    res.json({ success: false, message: err.errors.email.message }); // Return error
+                  } else {
+                    // Check if validation error is in the username field
+                    if (err.errors.username) {
+                      res.json({ success: false, message: err.errors.username.message }); // Return error
+                    } else {
+                      // Check if validation error is in the password field
+                      if (err.errors.password) {
+                        res.json({ success: false, message: err.errors.password.message }); // Return error
+                      } else {
+                        res.json({ success: false, message: err }); // Return any other error not already covered
+                      }
+                    }
+                  }
+                } else {
+                  res.json({ success: false, message: 'Could not save user. Error: ', err }); // Return error if not related to validation
+                }
+              }
+            } else {
+              res.json({ success: true, message: 'Acount registered!' }); // Return success
+            }
+          });
+        }
+      }
+    }
+  });
 
-		  user.save(function(err) {
-
-		  	if (err){
-
-		  		if (err.code === 11000){
-
-		  			res.json({success: false, message: 'Username or e-mail already exists!'});
-
-		  		} else {
-
-		  			res.json({ success: false, message: "Could not save user. Error: ", err});
-		  		}   
-
-		  	} else {
-
-		  	  	 res.json({ success: true, message: "User Saved!"});
-
-		  	  	 console.log(user.username + ' ' + user.email + ' ' + user.password);
-		  	  }
-
-		  });
-		}
-	  }
-	}
-});
-
-	return router; // Return router object to main index.js
+  return router; // Return router object to main index.js
 }
